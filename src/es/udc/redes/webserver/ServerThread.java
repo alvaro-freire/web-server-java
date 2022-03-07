4c7b078937e5d03ad200af13acd5daa44a3c75fa
alvaro-freire <alvaro.freirea@udc.es>
2022-03-07 17:02:48 +0100

    start ServerThread

--- src/es/udc/redes/webserver/ServerThread.java
+++ src/es/udc/redes/webserver/ServerThread.java
@@ -15,16 +15,38 @@ public class ServerThread extends Thread {
 
     public void run() {
         try {
-          // This code processes HTTP requests and generates 
-          // HTTP responses
-          // Uncomment next catch clause after implementing the logic
-          //
-        // } catch (SocketTimeoutException e) {
-        //    System.err.println("Nothing received in 300 secs");
+            // This code processes HTTP requests and generates
+            // HTTP responses
+
+            // Set the input channel
+            InputStream input = socket.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+
+            // Set the output channel
+            OutputStream output = socket.getOutputStream();
+            PrintWriter writer = new PrintWriter(output, true);
+
+            // Receive the message from the client
+            String message = reader.readLine();
+            String[] msgArray = message.split(" ");
+            String path = msgArray[1];
+
+
+
+            // Close the streams
+            input.close();
+            output.close();
+        } catch (SocketTimeoutException e) {
+           System.err.println("Nothing received in 300 secs");
         } catch (Exception e) {
             System.err.println("Error: " + e.getMessage());
         } finally {
             // Close the client socket
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
